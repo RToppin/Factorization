7508e1209b05517da2102bdd0e678e99394b1440
RToppin <ryan.toppin3408@gmail.com>
2025-04-04 16:33:49 -0400

    Added DistributedSolver.java

---
+++ src/DistributedSolver.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+
+public class DistributedSolver extends Solver{
+
+    @Override
+    public void precompute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Integer> getFactors(int n) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
